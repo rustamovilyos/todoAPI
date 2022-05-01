f772c699169e0e6b85c7f7eb6424400da4165ecf
rustamovilyos <rustamov7871@gmail.com>
2022-05-01 17:01:02 +0300

    added serializers class

--- mytask/models.py
+++ mytask/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    data = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
